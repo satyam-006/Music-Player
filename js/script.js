8ac300d48e903ecc1ffdeca23a0af55fe30998db
satyam-006 <satyamyt876@gmail.com>
2024-04-13 12:24:17 +0530

    console log

--- js/script.js
+++ js/script.js
@@ -60,6 +60,7 @@ async function getSongs(folder) {
 
 const playMusic = (track, pause = false) => {
     currentSong.src = `/${currFolder}/` + track
+    console.log(currentSong.src)
     if (!pause) {
         currentSong.play()
         play.src = "img/pause.svg"
